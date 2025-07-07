e16f5a2ed4b1facf863b4ce037afc88d3d7599c6
WP-digvijay <rajputdigvijay108@gmail.com>
2025-07-07 12:46:59 +0530

    create accessoriescategory  based plugin

---
+++ README.md
@@ -0,0 +1,38 @@
+# Woo Addon Discount
+
+Woo Addon Discount is a WordPress plugin for WooCommerce that automatically applies a 15% discount on accessories when the cart value reaches ₹2000 or more. The plugin does not use coupon codes, the discount is applied automatically.
+
+## Features
+
+- Automatically applies a 15% discount on accessories.
+- Discount triggers when cart value is ₹2000 or above.
+- No coupon code effected it get total cart value.
+- Seamless integration with WooCommerce.
+
+## Installation
+
+1. Download the plugin ZIP file or clone the repository.
+2. Upload the `woo-addon-discount` folder to the `/wp-content/plugins/` directory.
+3. Activate the plugin through the 'Plugins' menu in WordPress.
+4. Ensure your products are categorized as "Accessories" for the discount to apply.
+
+## Installation (CMS Based)
+
+1. Download the plugin ZIP file or clone the repository.
+2. Go to Admin Dashboard and add new plugin
+3. Upload Zip file and activate Plugin
+4. Ensure your products are categorized as "Accessories" for the discount to apply.
+
+## Usage
+
+- Add products to your cart.
+- When the Accessories total reaches ₹2000 or more, a 15% discount will be automatically applied to all accessories in the cart.
+- No further configuration is required.
+
+## Support
+
+For support or feature requests, please open an issue on the plugin repository.
+
+## tested
+- WooCommerce v(9.9.3)
+- Wordpress v(6.8.1)
---
+++ woo-addon-discount.php
@@ -0,0 +1,89 @@
+<?php
+    /**
+     * Plugin Name: WooCommerce Addon Discount
+     * Description: A plugin to add a discount on WooCommerce products.
+     * Version: 1.0.0
+     * Author: digvijay
+     * Author URI: https://wp-digvijay.github.io/Portfolio/
+     */
+    if(!defined('ABSPATH')){
+        exit; 
+    }
+    if(!class_exists('Rd_Woo_Accessories_Discount')){
+        class Rd_Woo_Accessories_Discount{
+            private $discount_amount = 0;
+            private $discount_lable = 'Accessorices Discount';
+
+            public function __construct(){
+                add_action('woocommerce_cart_calculate_fees', [$this, 'apply_discount']);
+                add_action('woocommerce_review_order_before_cart_contents', [$this, 'display_discount_label']);
+            }
+            //this one work when one accessories product in cart total cart value above or equal to 2000
+            // public function apply_discount($cart){
+            //     if(is_admin() && !defined('DOING_AJAX')){
+            //         return;
+            //     }
+            //     $accessories = false;
+            //     $cart_total  = $cart->get_subtotal();
+                
+            //     foreach($cart->get_cart() as $cart_item){
+            //         $product_id = $cart_item['product_id'];
+            //         $terms = get_the_terms($product_id, 'product_cat', true);
+
+            //         if($terms && !is_wp_error($terms)){
+            //             foreach($terms as $term){
+            //                 if(strtolower($term->name) === 'accessories'){
+            //                     $accessories = true;
+            //                     break 2;
+            //                 }
+            //             }
+            //         }
+            //     }
+
+            //     if($accessories && $cart_total >= 2000){
+            //         $discount = $cart_total * 0.15;
+            //         $cart->add_fee($this->discount_lable, -$discount, false);
+            //         WC()->session->set('accessories_discount_applied', true);
+            //     }
+            //     else{
+            //         WC()->session->__unset('accessories_discount_applied');
+            //     }
+            // }
+            
+            //this one work when accessories product in cart total accessories product value ablove or equal to 2000
+            public function apply_discount($cart){
+                if(is_admin() && !defined('DOING_AJAX')){
+                    return;
+                }
+                $accessories_subtotal = 0;
+                $has_accessories = false;
+                foreach($cart->get_cart() as $cart_item){
+                    $product_id = $cart_item['product_id'];
+                    $terms = get_the_terms($product_id, 'product_cat, true);');
+                    if($terms && !is_wp_error($terms)){
+                        foreach($terms as $terms){
+                            if(strtotalower($term->name) === 'accessories'){
+                                $has_accessories = true;
+                                $accessories_subtotal += $cart_item['Line_total'];
+                            }
+                        }
+                    }
+                }
+                if($has_accessories && $accessories_subtotal >= 2000){
+                    $discount = $accessories_subtotal * 0.15;
+                    $cart->add_fee($this->discount_lable, -$discount, false);
+                    WC()->session->set('accessories_discount_applied', true);
+                }
+                else{
+                    WC()->session->__unset('accessories_discount_applied');
+                }
+            }
+            public function display_discount_label(){
+                if(WC()->session->get('accessories_discount_applied')){
+                    wc_print_notice('You’ve received a 15% Accessories Discount!', 'success');
+                }
+            }
+        }
+        new Rd_Woo_Accessories_Discount();
+    }
+    
